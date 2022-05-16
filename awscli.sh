c3fff598f1a09fd9db9b7f9053f259c5b16bc7d0
Andre Fernandes <andre.fernandes86@gmail.com>
2022-05-16 10:40:30 -0500

    Update awscli.sh

--- awscli.sh
+++ awscli.sh
@@ -4,7 +4,7 @@ wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 
 unzip awscli-bundle.zip
 
-sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 
 ./awscli-bundle/install -b ~/bin/aws
 
